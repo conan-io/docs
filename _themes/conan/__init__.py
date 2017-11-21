2ce7032f500e1b20c69b9f76426269dcebb14f74
Luis Martinez de Bartolome <lasote@gmail.com>
2017-11-21 14:13:19 +0100

    Added sitemap.xml

--- _themes/conan/__init__.py
+++ _themes/conan/__init__.py
@@ -3,13 +3,47 @@
 From https://github.com/ryan-roemer/sphinx-bootstrap-theme.
 
 """
+
 import os
+import xml.etree.ElementTree as ET
 
 __version__ = '0.2.0'
 __version_full__ = __version__
 
 
+def setup(app):
+    """Setup conntects events to the sitemap builder"""
+    app.connect('html-page-context', add_html_link)
+    app.connect('build-finished', create_sitemap)
+    app.sitemap_links = []
+
 def get_html_theme_path():
     """Return list of HTML theme paths."""
     cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    return cur_dir
+    return [cur_dir]
+
+def add_html_link(app, pagename, templatename, context, doctree):
+    """As each page is built, collect page names for the sitemap"""
+    base_url = app.config['html_theme_options'].get('base_url', '')
+    if base_url:
+        app.sitemap_links.append(base_url + pagename + ".html")
+
+
+def create_sitemap(app, exception):
+    """Generates the sitemap.xml from the collected HTML page links"""
+    if (not app.config['html_theme_options'].get('base_url', '') or
+           exception is not None or
+           not app.sitemap_links):
+        return
+
+    filename = app.outdir + "/sitemap.xml"
+    print("Generating sitemap.xml in %s" % filename)
+
+    root = ET.Element("urlset")
+    root.set("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9")
+
+    for link in app.sitemap_links:
+        url = ET.SubElement(root, "url")
+        ET.SubElement(url, "loc").text = link
+
+    ET.ElementTree(root).write(filename)
--- conf.py
+++ conf.py
@@ -18,7 +18,9 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath('./_themes'))
+
 
 # -- General configuration ------------------------------------------------
 
@@ -30,6 +32,7 @@ import os
 # ones.
 extensions = [
     'sphinx.ext.todo',
+    'conan',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -106,18 +109,21 @@ todo_include_todos = True
 
 
 # -- Options for HTML output ----------------------------------------------
+import conan
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'sphinx_rtd_theme'
 html_theme = "conan"
-html_theme_path = ["_themes", ]
+html_theme_path = conan.get_html_theme_path()
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+  "base_url": "http://docs.conan.io/en/latest/"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
