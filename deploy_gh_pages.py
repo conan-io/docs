74e0ca67fec96562f9894c2cbef92e8abcc8d328
danimtb <danimanzaneque@gmail.com>
2018-07-17 16:17:47 +0200

    Add sphinx commands to generate pdfs and travis deps

--- deploy_gh_pages.py
+++ deploy_gh_pages.py
@@ -46,9 +46,11 @@ def build_and_copy(branch, folder_name, versions_available, validate_links=False
     call("make html")
     if validate_links:
         call("make linkcheck")
+    call("make latexpdf")
     tmp_dir = tempfile.mkdtemp()
 
     copytree("_build/html/", tmp_dir)
+    copytree("_build/latex/conan.pdf", tmp_dir)
     shutil.rmtree("_build")
 
     # Go to deploy branch, copy new files and commit
