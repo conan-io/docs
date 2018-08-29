4f7f8446592f9c45b2f9e7cd97fb4dc5a39c1b0b
Luis Martinez de Bartolome <lasote@gmail.com>
2018-08-29 11:32:36 +0200

    1.7.1

--- conf.py
+++ conf.py
@@ -38,9 +38,9 @@ extensions = [
 ]
 
 # The short X.Y version.
-version = "1.6"
+version = "1.7"
 # The full version, including alpha/beta/rc tags.
-release = u'1.6.1'
+release = u'1.7.0'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(os.path.join(dir_path, "versions.json")):
