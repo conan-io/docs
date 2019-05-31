f24e9e82fec4365faf1273431b2acdbc5d76edf2
Luis Martinez de Bartolome <lasote@gmail.com>
2019-05-31 14:01:48 +0200

    Replaced old GA code

--- replace_analytics_code.py
+++ replace_analytics_code.py
@@ -1,34 +1,15 @@
 import os
+import subprocess
 
-branches = ["release/1.10.0", "release/1.10.1",
-            "release/1.10.2",
-            "release/1.11.0",
-            "release/1.11.1",
-            "release/1.11.2",
-            "release/1.12.0",
-            "release/1.12.1",
-            "release/1.12.1",
-            "release/1.12.3",
-            "release/1.13.0",
-            "release/1.13.1",
-            "release/1.13.2",
-            "release/1.13.3",
-            "release/1.14.0",
-            "release/1.14.1",
-            "release/1.14.2",
-            "release/1.14.3",
-            "release/1.14.4",
-            "release/1.14.5",
-            "release/1.15.0",
-            "release/1.15.5",
-            "release/1.3.3",
-            "release/1.4.5",
-            "release/1.5.2",
-            "release/1.6.1",
-            "release/1.7.4",
-            "release/1.8.5",
-            "release/1.9.4"]
+lines = subprocess.check_output(['git', 'branch', "-a"]).decode().split("\n")
 
+branches = []
+for line in lines:
+    line = line.strip()
+    if line.startswith("remotes/origin/release/"):
+        branches.append(line.split("remotes/origin/")[1])
+
+print(branches)
 
 old_code = "GTM-WK44ZFM"
 old_code_2 = "GTM-WK44ZFM"
