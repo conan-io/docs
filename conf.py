50df413573c6129c7deae91e98e15a28f462742b
czoido <mrgalleta@gmail.com>
2021-10-06 13:36:31 +0200

    conan 1.41.0

--- conf.py
+++ conf.py
@@ -41,9 +41,9 @@ extensions = [
 ]
 
 # The short X.Y version.
-version = "1.40"
+version = "1.41"
 # The full version, including alpha/beta/rc tags.
-release = u'1.40.4'
+release = u'1.41.0'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(os.path.join(dir_path, "versions.json")):
