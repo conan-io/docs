aec06b6e0cee8919830415bcedf13c77efc70133
Carlos Zoido <mrgalleta@gmail.com>
2022-02-03 18:34:01 +0100

    Fix broken links in documentation (#2389)
    * remove anchor
    
    * ignore bintray

--- conf.py
+++ conf.py
@@ -393,7 +393,7 @@ epub_exclude_files = ['search.html']
 #epub_use_index = True
 
 # A list of regular expressions that match URIs that should not be checked when doing a linkcheck build.
-linkcheck_ignore = [r'http://localhost:\d+', r'https://github.com/conan-io/conan/pull/\d+', r'https://github.com/conan-io/docs/pull/\d+']
+linkcheck_ignore = [r'http://localhost:\d+', r'https://github.com/conan-io/conan/pull/\d+', r'https://github.com/conan-io/docs/pull/\d+', 'https://conan.bintray.com']
 linkcheck_workers = 15
 linkcheck_timeout = 90
 linkcheck_retries = 2
