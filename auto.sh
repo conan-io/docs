b2c08c808b435ea33119f447e1ab31a3aaa9017b
Francisco Ramírez <franchuti688@gmail.com>
2023-02-10 11:59:43 +0100

    Improved (#2960)

--- auto.sh
+++ auto.sh
@@ -1,5 +1,8 @@
+# Don't wait till any change is done
+echo Building HTML files && make html
 watchmedo shell-command \
     --patterns="*.rst" \
     --recursive \
     --command='echo Rebuilding && make html' \
+    --drop
     .
