2b3b09f109bb8e1c9d5b4a392dfc0f247ebc640a
czoido <mrgalleta@gmail.com>
2023-02-28 09:42:09 +0100

    minor changes

--- .ci/scripts/get_branches.py
+++ .ci/scripts/get_branches.py
@@ -4,14 +4,12 @@ from common import run, conan_versions
 
 
 """
-    get the branches we have to build the docs for, there are two scenarios:
+    Get the branches we have to build the docs for, there are two scenarios:
 
     1. We changed something in the .ci scripts or change the _themes folder in the master
     branch -> regenerate every branch of the docs.
 
     2. If we did not touch those folders just regenerate the branch we pushed
-
-    returns None if we have to build all branches or the branch name that we have to build
 """
 current_branch = os.getenv("BRANCH_NAME")
 
