000f2f245dc8ea7999e18104cda8d8c8f4d77de9
Carlos Zoido <mrgalleta@gmail.com>
2023-02-28 09:48:50 +0100

    Improve the way the docs are published (#3030)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * restore publish
    
    * wip
    
    * remove
    
    * wip
    
    * minor changes
    
    * minor changes

---
+++ .ci/scripts/prepare_gh_pages.py
@@ -0,0 +1,67 @@
+import os
+import argparse
+from pathlib import Path
+
+from common import chdir, run, latest_v1_folder, latest_v2_folder, latest_v2_version
+from create_redirects import create_redirects
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--sources-folder',
+                    help='Folder where the docs were created', required=True)
+
+parser.add_argument('--gh-pages-folder',
+                    help='Folder to clone the gh-pages branch to', required=True)
+
+args = parser.parse_args()
+
+output_folder = os.path.join(args.sources_folder, "output")
+pages_folder = args.gh_pages_folder
+
+with chdir(output_folder):
+    # FIXME: this is to not break all links from https://docs.conan.io/en/latest/
+    # we copy all the /1 folder to /en/latest and then replace all html files
+    # there with redirects to https://docs.conan.io/en/latest/1
+    # remove when most of the traffic in the docs is for 2.X docs
+
+    # First check if we generated any docs in `latest_v1_folder`
+    path_latest_v1 = Path(os.path.join(latest_v1_folder))
+    if path_latest_v1.exists():
+        run('mkdir -p en/latest')
+        run(f"cp -R {latest_v1_folder}/* en/latest")
+        create_redirects(path_html="en/latest", old_slug="en/latest", new_slug="1")
+
+    # 2 folder is the same as the latest 2.X, copy the generated html files to 2 folder
+    path_latest_v2 = Path(os.path.join(latest_v2_version))
+    if path_latest_v2.exists():
+        run(f"cp -R {latest_v2_version} {latest_v2_folder}")
+
+#run(f"rm -rf {pages_folder}")
+
+docs_repo_url = 'https://github.com/conan-io/docs.git'
+run(f"git clone --single-branch -b gh-pages --depth 1 {docs_repo_url} {pages_folder}")
+
+run(f"cp -R {output_folder}/* {pages_folder}")
+
+run(f"cp {output_folder}/{latest_v2_folder}/404.html {pages_folder}/404.html")
+
+path_404 = f"{pages_folder}/404.html"
+
+with open(path_404, 'r') as file_404 :
+  contents_404 = file_404.read()
+
+prefix = 'https://docs.conan.io'
+prefix_latest = f"{prefix}/{latest_v2_folder}"
+
+contents_404 = contents_404.replace('href="_', f"href=\"{prefix_latest}/_")
+contents_404 = contents_404.replace('src="_', f"src=\"{prefix_latest}/_")
+contents_404 = contents_404.replace('alt="_', f"alt=\"{prefix_latest}/_")
+contents_404 = contents_404.replace('internal" href="', f"internal\" href=\"{prefix_latest}/")
+contents_404 = contents_404.replace('"search.html"', f"\"{prefix_latest}/search.html\"")
+contents_404 = contents_404.replace('"genindex.html"', f"\"{prefix_latest}/genindex.html\"")
+
+with open(path_404, 'w') as file:
+    file.write(contents_404)
+
+# gh-pages prepared to push
