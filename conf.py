fff68b16c8d0c7fcd0b1175fb3baea15edcf7546
czoido <mrgalleta@gmail.com>
2023-03-09 10:39:28 +0100

    use png for pdf gen

--- conf.py
+++ conf.py
@@ -282,7 +282,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = "_static/conan-docs-logo-2.svg"
+latex_logo = "_static/conan-docs-logo-2.png"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
