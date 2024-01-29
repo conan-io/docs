f3b8f64a8525ef8adf893393f7220a9d37f6a3ff
czoido <mrgalleta@gmail.com>
2024-01-29 17:23:49 +0100

    build pdf with -f
    (cherry picked from commit 7d2de88a0f333ed9be8f201cb7beb49b47cc8a79)

--- .ci/scripts/generate_documentation.py
+++ .ci/scripts/generate_documentation.py
@@ -69,6 +69,7 @@ with chdir(f"{sources_folder}"):
 
     # generate pdf
     if with_pdf:
-        run(f"sphinx-build -W -b latex -d {branch_folder}/_build/.doctrees {branch_folder}/ {branch_folder}/_build/latex")
+        # adding the -f flag because the pdf gen fails for some versions
+        run(f"sphinx-build -W -b latex -f -d {branch_folder}/_build/.doctrees {branch_folder}/ {branch_folder}/_build/latex")
         run(f"make -C {branch_folder}/_build/latex all-pdf")
         run(f"cp {branch_folder}/_build/latex/conan.pdf {output_folder}/{branch_folder}/conan.pdf")
