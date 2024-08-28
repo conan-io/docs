c47947375613fe469ed1b6cdc87481db1ba60076
czoido <mrgalleta@gmail.com>
2024-08-28 18:00:54 +0200

    Conan 2.7.0 docs

--- conf.py
+++ conf.py
@@ -64,9 +64,9 @@ autodoc_mock_imports = ["PyJWT", "requests", "urllib3", "PyYAML",
 
 
 # The short X.Y version.
-version = "2.6"
+version = "2.7"
 # The full version, including alpha/beta/rc tags.
-release = u'2.6.0'
+release = u'2.7.0'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(os.path.join(dir_path, "versions.json")):
