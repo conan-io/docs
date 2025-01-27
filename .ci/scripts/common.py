8f3742e9b2dc7e9bad017d20deb182c387ca3f01
czoido <mrgalleta@gmail.com>
2025-01-27 17:43:34 +0100

    Conan 2.12

--- .ci/scripts/common.py
+++ .ci/scripts/common.py
@@ -10,6 +10,7 @@ latest_v1_branch = "master"
 conan_versions = {
     # the first of the dictionary
     # must be always the latest version
+    "2.12": "release/2.12",
     "2.11": "release/2.11",
     "2.10": "release/2.10",
     "2.9": "release/2.9",
