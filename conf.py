7b1170c961e32a53985827269276767bf811faee
Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>
2025-09-29 12:33:28 +0200

    Revert changes that should go to master

--- conf.py
+++ conf.py
@@ -42,6 +42,7 @@ sys.path.append(os.path.abspath('./_ext'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxcontrib.spelling',
     'sphinx_sitemap',
     'notfound.extension',
     'sphinx.ext.autodoc',
