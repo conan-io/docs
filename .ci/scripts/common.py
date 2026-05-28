6b2d6e9e6e584a948965ce4667a783c779797004
Carlos Zoido <mrgalleta@gmail.com>
2026-05-28 18:12:18 +0200

    add Conan 2.29

--- .ci/scripts/common.py
+++ .ci/scripts/common.py
@@ -10,6 +10,7 @@ latest_v1_branch = "master"
 conan_versions = {
     # the first of the dictionary
     # must be always the latest version
+    "2.29": "release/2.29",
     "2.28": "release/2.28",
     "2.27": "release/2.27",
     "2.26": "release/2.26",
